77fcfbff0c5cdc3695d171963c2448f796ed2dc2
Samie-ub <xxxsamie39@gmail.com>
2023-12-30 22:16:46 +0500

    updated code 2

--- script/main.js
+++ script/main.js
@@ -1,106 +1,129 @@
-const loader = document.getElementById('loader');
-    setTimeout(() => {
-      loader.style.display = 'none';
-    }, 5000);
+const loader = document.getElementById("loader");
+setTimeout(() => {
+  loader.style.display = "none";
+}, 3000);
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const imageInput = document.getElementById('imageInput');
-        const colorContainer = document.getElementById('colorContainer');
-        const addBtn = document.querySelector('.add-btn img');
-    
-        let colorContainerVisible = false; // Track the visibility state
-    
-        addBtn.addEventListener('click', handleButtonClick);
-    
-        function handleButtonClick() {
-            if (colorContainerVisible) {
-                // Remove colorContainer from the DOM
-                colorContainer.innerHTML = '';
-                colorContainer.style.display = 'none';
-    
-                // Reset button icon to "add.png"
-                addBtn.src = './public/add.png';
-    
-                // Reset visibility state
-                colorContainerVisible = false;
-            } else {
-                // Trigger the file input by clicking on it
-                imageInput.click();
-            }
-        }
-    
-        imageInput.addEventListener('change', handleImageUpload);
-    
-        function handleImageUpload() {
-            const file = imageInput.files[0];
-    
-            if (file) {
-                // Update button icon to "back.png"
-                addBtn.src = './public/back.png';
-    
-                // Hide colorContainer
-                colorContainer.style.display = 'none';
-    
-                loadImage(file)
-                    .then(image => extractColors(image))
-                    .catch(error => console.error('Error loading image:', error));
-            }
-        }
-    
-        function loadImage(file) {
-            return new Promise((resolve, reject) => {
-                const reader = new FileReader();
-    
-                reader.onload = function (e) {
-                    const image = new Image();
-                    image.src = e.target.result;
-    
-                    image.onload = function () {
-                        resolve(image);
-                    };
-    
-                    image.onerror = function () {
-                        reject('Failed to load the image.');
-                    };
-                };
-    
-                reader.readAsDataURL(file);
-            });
-        }
-    
-        function extractColors(image) {
-            const vibrant = new Vibrant(image);
-            const swatches = vibrant.swatches();
-    
-            if (swatches && Object.keys(swatches).length > 0) {
-                const colorValues = Object.values(swatches)
-                    .filter(swatch => swatch !== undefined) // Filter out undefined swatches
-                    .map(swatch => swatch.getHex());
-    
-                if (colorValues.length > 0) {
-                    displayColors(colorValues);
-    
-                    // Show colorContainer
-                    colorContainer.style.display = 'flex';
-                    // Update visibility state
-                    colorContainerVisible = true;
-                } else {
-                    console.error('No valid color swatches found.');
-                }
-            } else {
-                console.error('Unable to extract colors from the image.');
-            }
-        }
-    
-        function displayColors(colors) {
-            // Display colors in colorContainer
-            colorContainer.innerHTML = '';
-            colors.forEach(color => {
-                const colorDiv = document.createElement('div');
-                colorDiv.style.backgroundColor = color;
-                colorDiv.textContent = color;
-                colorContainer.appendChild(colorDiv);
-            });
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const imageInput = document.getElementById("imageInput");
+  const colorContainer = document.getElementById("colorContainer");
+  const addBtn = document.querySelector(".add-btn img");
+
+  let colorContainerVisible = false;
+
+  addBtn.addEventListener("click", handleButtonClick);
+
+  function handleButtonClick() {
+    if (colorContainerVisible) {
+      colorContainer.innerHTML = "";
+      colorContainer.style.display = "none";
+
+      addBtn.src = "./public/add.png";
+
+      colorContainerVisible = false;
+    } else {
+      imageInput.click();
+    }
+  }
+
+  imageInput.addEventListener("change", handleImageUpload);
+
+  function handleImageUpload() {
+    const file = imageInput.files[0];
+
+    if (file) {
+      addBtn.src = "./public/back.png";
+      colorContainer.style.display = "none";
+
+      loadImage(file)
+        .then((image) => extractColors(image))
+        .catch((error) => console.error("Error loading image:", error));
+    }
+  }
+
+  function loadImage(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        const image = new Image();
+        image.src = e.target.result;
+
+        image.onload = function () {
+          resolve(image);
+        };
+
+        image.onerror = function () {
+          reject("Failed to load the image.");
+        };
+      };
+
+      reader.readAsDataURL(file);
     });
-    
+  }
+
+  function extractColors(image) {
+    const vibrant = new Vibrant(image);
+    const swatches = vibrant.swatches();
+
+    if (swatches && Object.keys(swatches).length > 0) {
+      const colorValues = Object.values(swatches)
+        .filter((swatch) => swatch !== undefined)
+        .map((swatch) => swatch.getHex());
+
+      if (colorValues.length > 2) {
+        displayColors(colorValues);
+
+        colorContainer.style.display = "flex";
+        colorContainerVisible = true;
+      } else {
+        alert("Image must contain at least 2 valid color swatches.");
+        colorContainer.style.display = "none";
+
+        colorContainerVisible = false;
+        location.reload();
+      }
+    } else {
+      console.error("Unable to extract colors from the image.");
+    }
+  }
+
+  function displayColors(colors) {
+    colorContainer.innerHTML = "";
+    colors.forEach((color) => {
+      const colorDiv = document.createElement("div");
+      colorDiv.style.backgroundColor = color;
+      colorDiv.textContent = color;
+      colorDiv.addEventListener("click", () => handleColorClick(color));
+      colorContainer.appendChild(colorDiv);
+    });
+  }
+
+  function handleColorClick(color) {
+    showCopiedSign(event, color);
+  }
+
+  function showCopiedSign(event, color) {
+    const copiedSign = document.createElement("div");
+    copiedSign.classList.add("copied-sign");
+    copiedSign.textContent = "Copied!";
+    copiedSign.style.top = event.clientY + "px";
+    copiedSign.style.left = event.clientX + "px";
+
+    document.body.appendChild(copiedSign);
+
+    setTimeout(() => {
+      document.body.removeChild(copiedSign);
+    }, 1000);
+
+    copyToClipboard(color);
+  }
+
+  function copyToClipboard(text) {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+});
